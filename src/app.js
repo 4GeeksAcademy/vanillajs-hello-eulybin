34dfb0e694f414cb2538ce0f7b8b1dac73dd5a45
Egor Ulybin <egorulybin@gmail.com>
2025-07-18 19:33:35 +0000

    project delivery

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  const dataObj = {
+    prefix: ['https://'],
+    pronoun: ['the', 'our'],
+    adj: ['great', 'big'],
+    noun: ['jogger', 'racoon'],
+    extension: ['.com', '.net'],
+  };
 
+  const outputElem = document.getElementById('domain-name-output');
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const generateDomainName = ({ prefix, pronoun, adj, noun, extension }) => {
+    let result = '';
+    for (let h = 0; h < prefix.length; h++) {
+      for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+          for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < extension.length; l++) {
+              result += `<p>${prefix[h]}${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}</p>`;
+            }
+          }
+        }
+      }
+    }
+    return (outputElem.innerHTML = result);
+  };
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateDomainName(dataObj);
 };
